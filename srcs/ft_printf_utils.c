bdef54da1fc0f49d740afb21866bad5f86f7bcbc
kmazier <kmazier@student.42.fr>
2020-12-02 10:37:03 +0100

    refix

--- srcs/ft_printf_utils.c
+++ srcs/ft_printf_utils.c
@@ -6,7 +6,7 @@
 /*   By: kmazier <kmazier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 04:27:08 by kmazier           #+#    #+#             */
-/*   Updated: 2020/12/02 10:32:49 by kmazier          ###   ########.fr       */
+/*   Updated: 2020/12/02 10:36:41 by kmazier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,9 @@ void	ft_print_address(unsigned long n)
 {
 	char *hex = "0123456789abcdef";
 	
-	if (n > 15)
+	if (n != 0)
+	{
 		ft_print_address(n / 16);
-	ft_putchar_fd(hex[n % 16] ,1);
+		ft_putchar_fd(hex[n % 16] ,1);
+	}
 }
--- srcs/ft_putnbr_base.c
+++ srcs/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: kmazier <kmazier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/11 11:17:01 by kmazier           #+#    #+#             */
-/*   Updated: 2020/12/02 10:33:43 by kmazier          ###   ########.fr       */
+/*   Updated: 2020/12/02 10:36:52 by kmazier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,12 +37,12 @@ int		ft_check_base_nbr(char *base)
 
 void	ft_write(int nb, char *base, unsigned int blen)
 {
-	if (((unsigned int)nb) > 0)
+	if (((unsigned int)nb) > blen - 1)
 	{
 		ft_write(nb / blen, base, blen);
-		ft_putchar_fd(base[nb % blen], 1);
+		nb %= blen;
 	}
-	
+	ft_putchar_fd(base[nb], 1);
 }
 
 void	ft_putnbr_base(int nbr, char *base)
