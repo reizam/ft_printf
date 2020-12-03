a2445860ceb8a07786b9f5147c95ac8851639022
kmazier <kmazier@student.42.fr>
2020-12-03 07:48:43 +0100

    fix

--- srcs/ft_printf_flags.c
+++ srcs/ft_printf_flags.c
@@ -6,7 +6,7 @@
 /*   By: kmazier <kmazier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 07:48:23 by kmazier           #+#    #+#             */
-/*   Updated: 2020/12/03 07:45:25 by kmazier          ###   ########.fr       */
+/*   Updated: 2020/12/03 07:48:37 by kmazier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	ft_calc_zero(t_flags *flags, size_t *len, long int nbr)
 		len--;
 	}
 	diff = flags->amount_set == 1 ? flags->amount_show - (*len - (nbr < 0)) : flags->left_zero - *len;
-	if (flags->amount_set == 1 && nbr == 0 && flags->lzero_set)
+	if (flags->amount_set == 1 && nbr <= 9 && nbr >= -9 && flags->lzero_set)
 		flags->spaces = flags->left_zero;
 	while (diff > 0)
 	{
