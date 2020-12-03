493c8cc87626623fc525e4e96bc35172fd31ffbe
kmazier <kmazier@student.42.fr>
2020-12-03 09:49:25 +0100

    fix

--- srcs/ft_printf_type.c
+++ srcs/ft_printf_type.c
@@ -6,7 +6,7 @@
 /*   By: kmazier <kmazier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 04:27:45 by kmazier           #+#    #+#             */
-/*   Updated: 2020/12/03 09:47:02 by kmazier          ###   ########.fr       */
+/*   Updated: 2020/12/03 09:49:12 by kmazier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ void	ft_print_arg_int(va_list *ap, t_flags *flags, size_t *length)
 	nbr = (long int)va_arg(*ap, int);
 	len = ft_strlen((str = ft_itoa(nbr)));
 	j = ft_calc_zero(flags, nbr, len) + (nbr == 0 && flags->amount_set == 1 ? 0 : 1);
-	ft_print_flags(flags, 0, len, length);
 	len += j;
+	ft_print_flags(flags, 0, len, length);
 	if (str[0] && str[0] == '-')
 	{
 		ft_putchar_fd('-', 1);
